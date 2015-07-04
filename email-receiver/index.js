9da2f79c51ec6f1dc3dbbabb8b8f8f94f752404a
Ken Grønnbeck <kengroenn@gmail.com>
2015-07-04 12:39:43 +0200

    First commit

---
+++ email-receiver/index.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var multer = require('multer');
+var EventStore = require('event-store-client');
+var ExpectedVersion = EventStore.ExpectedVersion;
+
+var PORT = process.env.PORT || 8080;
+var EVENTSTORE_HOST = process.env.EVENTSTORE_HOST || '192.168.59.103';
+var EVENTSTORE_PORT = process.env.EVENTSTORE_PORT || 2113
+
+var app = express();
+app.use(multer());
+
+var options = {
+  host: EVENTSTORE_HOST
+}
+
+var connection = new EventStore.Connection(options)
+var credentials = {
+  username: 'admin',
+  password: 'changeit'
+}
+
+function saveMailEvent (event, callback) {
+  connection.writeEvents('emails', ExpectedVersion.Any, false,  [event],
+    credentials, function (completed) {
+      console.log('Events written result: ' + EventStore.OperationResult.getName(completed.result));
+      if (completed.error) {
+        callback(completed.error);
+      } else {
+        callback(null, completed.result);
+      }
+    });
+}
+
+function createGuid () {
+  return EventStore.Connection.createGuid();
+}
+
+function createEvent(payload) {
+  return {
+    eventId: createGuid(),
+    eventType: 'mail/sendgrid+unstable',
+    data: payload
+  }
+}
+
+app.post('/', function (req, res) {
+  var payload = req.body;
+  var event = createEvent(payload);
+  saveMailEvent(event, function (err) {
+    if (err) return res.status(500).send({success: false})
+    res.send({success: true});
+  });
+});
+
+app.listen(PORT, function() { console.log('running'); });
---
+++ readme.md
@@ -0,0 +1,3 @@
+# RemindMe2
+
+Building a reminder app using an async architecture. 
